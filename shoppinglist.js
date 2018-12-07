4b506693149e34313a99434dc2fd7e63a7cc2459
Duy Le <duyvietle13@gmail.com>
2018-12-07 08:35:34 -0800

    competed rendering the shopping list

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// `STORE` is responsible for storing the underlying data
-// that our app needs to keep track of in order to work.
-//
-// for a shopping list, our data model is pretty simple.
-// we just have an array of shopping list items. each one
-// is an object with a `name` and a `checked` property that
-// indicates if it's checked off or not.
-// we're pre-adding items to the shopping list so there's
-// something to see when the page first loads.
 const STORE = [
   {name: "apples", checked: false},
   {name: "oranges", checked: false},
@@ -17,13 +8,36 @@ const STORE = [
 ];
 
 
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+
+function generateShoppingItemsString(shoppingList) {
+  console.log("Generating shopping list element");
+
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  
+  return items.join("");
+}
+
+
 function renderShoppingList() {
-  // this function will be responsible for rendering the shopping list in
-  // the DOM
   // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
-  const shoppingListItemsString = '<li>apples</li>';
-  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
 }
